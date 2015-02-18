7b6b75f636569c0e9b1125b67c10d8215542d483
Quan Hua <quanhua92@gmail.com>
2015-02-18 12:43:58 +0700

    Setup basic game skeleton

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -1,34 +1,81 @@
 #include "GameLayer.h"
+#include "SimpleAudioEngine.h"
 
 USING_NS_CC;
+using namespace CocosDenshion;
 
-Scene* GameLayer::createScene()
-{
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = GameLayer::create();
+Scene* GameLayer::createScene() {
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
 
-    // add layer as a child to scene
-    scene->addChild(layer);
+	// 'layer' is an autorelease object
+	auto layer = GameLayer::create();
 
-    // return the scene
-    return scene;
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
 }
 
 // on "init" you need to initialize your instance
-bool GameLayer::init()
-{
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
-    
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-    
-    return true;
+bool GameLayer::init() {
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init()) {
+		return false;
+	}
+
+	_visibleSize = Director::getInstance()->getVisibleSize();
+	_origin = Director::getInstance()->getVisibleOrigin();
+
+	createGameScreen();
+
+	resetGame();
+
+	// touch events
+	auto listener = EventListenerTouchAllAtOnce::create();
+
+	listener->onTouchesBegan = CC_CALLBACK_2(GameLayer::onTouchBegan, this );
+	listener->onTouchesEnded = CC_CALLBACK_2(GameLayer::onTouchEnded, this );
+
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener,
+			this);
+
+	// schedule update
+	this->scheduleUpdate();
+
+	return true;
+}
+
+void GameLayer::createGameScreen() {
+
+	Sprite *quickSprite = Sprite::create("blank.png");
+	quickSprite->setTextureRect(Rect(0, 0, 100, 100));
+	quickSprite->setColor(Color3B(255, 255, 255));
+
+	quickSprite->setPosition(Vec2(
+			_visibleSize.width * 0.5 + _origin.x,
+			_visibleSize.height * 0.5 + _origin.y
+	));
+
+	this->addChild(quickSprite);
+}
+
+void GameLayer::resetGame() {
+
+}
+
+void GameLayer::onTouchBegan(const std::vector<Touch*>& touches,
+		cocos2d::Event *event) {
+
+}
+
+void GameLayer::onTouchEnded(const std::vector<Touch*>& touches,
+		cocos2d::Event *event) {
+
+}
+
+void GameLayer::update(float dt){
+
 }
--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -3,15 +3,38 @@
 
 #include "cocos2d.h"
 
+USING_NS_CC;
+
+typedef enum {
+    kGameIntro,
+    kGamePlay,
+    kGameOver
+} GameState;
+
 class GameLayer : public cocos2d::Layer
 {
+private:
+	Size _visibleSize;
+	Vec2 _origin;
+	SpriteBatchNode* _gameBatchNode;
+
+	bool _running = false;
+	int _speedIncreaseInterval;
+	float _speedIncreaseTimer;
+
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-    
+    virtual void onTouchBegan(const std::vector<Touch*>& touches, cocos2d::Event *event);
+    virtual void onTouchEnded(const std::vector<Touch*>& touches, cocos2d::Event *event);
+
+    void update(float dt);
+    void createGameScreen();
+    void resetGame();
+
     // implement the "static create()" method manually
     CREATE_FUNC(GameLayer);
 };
