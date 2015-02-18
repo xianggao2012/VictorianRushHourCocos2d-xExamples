559c70c432102a2b93ca4b5c003f2e2563be423c
Quan Hua <quanhua92@gmail.com>
2015-02-18 15:40:47 +0700

    Add Terrain helper function

--- Classes/VictorianRushHour/Terrain.cpp
+++ Classes/VictorianRushHour/Terrain.cpp
@@ -1,30 +1,62 @@
 #include "Terrain.h"
 
-Terrain::Terrain(){
+Terrain::~Terrain () {
 
 }
 
-Terrain::~Terrain(){
+Terrain::Terrain():
+_startTerrain(false)
+,_blockPoolIndex(0)
+,_currentPatternCnt(1)
+,_currentPatternIndex(0)
+,_currentTypeIndex(0)
+,_currentWidthIndex(0)
+,_currentHeightIndex(0)
+,_showGap(false)
+{
+	_visibleSize = Director::getInstance()->getVisibleSize();
+	_origin = Director::getInstance()->getVisibleOrigin();
 
 }
 Terrain * Terrain::create() {
     Terrain * terrain = new Terrain();
 	if (terrain && terrain->initWithFile("blank.png")) {
-
         terrain->setAnchorPoint(Vec2(0,0));
-
         terrain->initTerrain();
-
-        terrain->autorelease();
-
-        return terrain;
+		terrain->autorelease();
+		return terrain;
 	}
-
-	CC_SAFE_DELETE(terrain);
 
 	return NULL;
 }
 
-void Terrain::initTerrain(){
+void Terrain::initTerrain () {
+
+}
+
+
+void Terrain::checkCollision (Player * player) {
+
+
+}
+
+void Terrain::move (float xMove) {
+
+}
+
+void Terrain::reset() {
+
+}
+
+void Terrain::addBlocks(int currentWidth) {
+
+}
+
+void Terrain::distributeBlocks() {
+
+}
+
+void Terrain::initBlock(Block * block) {
+
 
 }
--- Classes/VictorianRushHour/Terrain.h
+++ Classes/VictorianRushHour/Terrain.h
@@ -2,17 +2,67 @@
 #define __TERRAIN_H__
 
 #include "cocos2d.h"
-
+#include "Block.h"
+#include "Player.h"
 USING_NS_CC;
 
+#define TILE_H_SIZE 6
+#define TILE_W_SIZE 8
+
 class Terrain : public Sprite{
+	Vector<Block *> _blockPool;
+	int _blockPoolIndex;
+
+    Vector<Block *> _blocks;
+	int _lastBlockHeight;
+    int _lastBlockWidth;
+	int _minTerrainWidth;
+
+    bool _showGap;
+
+    int _currentPatternIndex;
+    int _currentPatternCnt;
+    int _currentWidthIndex;
+    int _currentHeightIndex;
+    int _currentTypeIndex;
+
+    int _increaseGapInterval;
+    float _increaseGapTimer;
+    int _gapSize;
+
+
+    Size _visibleSize;
+    Vec2 _origin;
+
+    void initTerrain ();
+    void addBlocks(int currentWidth);
+
+    void distributeBlocks();
+    void initBlock(Block * block);
+
+    inline float getWidth () {
+
+        int count = _blocks.size();
+        int width = 0;
+        Block * block;
+        for (int i = 0; i < count; i++) {
+            block = (Block *) _blocks.at(i);
+            width += block->getWidth();
+        }
+        return width;
+    }
 public:
 	Terrain();
 	~Terrain();
 
 	static Terrain * create();
 
-	void initTerrain();
+	CC_SYNTHESIZE(bool, _startTerrain, StartTerrain);
+
+    void checkCollision (Player * player);
+
+	void move (float xMove);
+    void reset (void);
 };
 
 #endif // __TERRAIN_H__
